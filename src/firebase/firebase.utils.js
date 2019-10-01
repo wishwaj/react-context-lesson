fe4dae1031ab7b7c36cd906cf47ea6bf35f31a4d
Wishwa Wickramasinghe <wishwaj@gmail.com>
2019-10-01 17:12:27 +0530

    firebase local configuration

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,13 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  apiKey: "AIzaSyCF-N6Ta6OcuvSXF8Yfx9kksk4kz11E55s",
+  authDomain: "crown-db-bef92.firebaseapp.com",
+  databaseURL: "https://crown-db-bef92.firebaseio.com",
+  projectId: "crown-db-bef92",
+  storageBucket: "",
+  messagingSenderId: "439049947256",
+  appId: "1:439049947256:web:74a3889ff5ccf7bb06058d"
 };
 
 firebase.initializeApp(config);
